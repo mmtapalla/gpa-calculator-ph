cfb202f6fa6aad47d080ef554bee04736530fcdd
mmtapalla <gm.tapallamiguel@gmail.com>
2024-07-23 13:16:29 +0800

    Reconfigure static files 6: Return of the King

--- gpaCalculatorPh/settings.py
+++ gpaCalculatorPh/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zm#=rx#_kaqvbrpv_z+q#v(o#o-9jpgz)=tvfbugggm-d6mk)j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
@@ -119,8 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
