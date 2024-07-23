a98b8ba7156db0f7106d0c00163e3b01e7866b6b
mmtapalla <gm.tapallamiguel@gmail.com>
2024-07-23 13:34:08 +0800

    Reconfigure static files 7: Fall of the King

--- gpaCalculatorPh/settings.py
+++ gpaCalculatorPh/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-zm#=rx#_kaqvbrpv_z+q#v(o#o-9jpgz)=tvfbugggm-d6mk)j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -76,12 +76,12 @@ WSGI_APPLICATION = 'gpaCalculatorPh.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
@@ -119,8 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
